e9d11fc9df10c8f6e47f2d74cba13b14c57be24b
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-03-28 23:10:30 +0300

    Create Day1_DataTypes.cs

---
+++ Day1_DataTypes.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Matan";
+            char Grade = 'A';
+            int Score = 100;
+            double FianlScore = 101.5;
+
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Your Grade: " + Grade);
+            Console.WriteLine("Score: " + Score);
+            Console.WriteLine("Your Final Score: " + FianlScore);
+            Console.ReadLine();
+        }
+    }
+}
