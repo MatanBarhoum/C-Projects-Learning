3aef24cfd5b5683a07aa7fc29a492f7d9dd80f43
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-03-28 22:39:27 +0300

    Create Day1_Variables.cs

---
+++ Day1_Variables.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string World = "World";
+            int WorldAge = 14;
+            Console.WriteLine("Hello " + World + " Your age is " + WorldAge + " Billions years old.");
+            Console.ReadLine();
+        }
+    }
+}
