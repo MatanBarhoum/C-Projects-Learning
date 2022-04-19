cd0bb968e6f0430f2e295755ea9d92355e74d005
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-04-19 21:31:58 +0300

    Add files via upload

---
+++ Control Flow Exercise 1/LearnwithMosh/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnwithMosh
+{
+    public static class Program
+    {
+        public static int NumberEntered;
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 1 - 10: ");
+            NumberEntered = Convert.ToInt32(Console.ReadLine());
+            ReturnIf.ValidationCheck();
+        }
+    }
+}
---
+++ Control Flow Exercise 1/LearnwithMosh/ReturnIf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnwithMosh
+{
+    public static class ReturnIf
+    {
+        public static void ValidationCheck()
+        {
+            if (Program.NumberEntered > 0 && Program.NumberEntered <= 10)
+            {
+                Console.WriteLine("Valid");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid");
+                Console.ReadLine();
+            }
+        }
+    }
+}
