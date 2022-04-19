530df24c49df49862a69889c9888eda94d5ca470
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-04-19 22:31:59 +0300

    Add files via upload

---
+++ Control Flow Exercise 3/LearnWithMosh1/Calculator.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnwithMosh
+{
+    public static class Calculator
+    {
+        public static void PortOrLand()
+        {
+            if (Program.width > Program.height)
+            {
+                Console.WriteLine("This image is Landscape");
+                Console.ReadLine();
+            }
+
+            else if (Program.height > Program.width)
+            {
+                Console.WriteLine("This image is Portrait");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry, not a landscape nor a portrait.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Control Flow Exercise 3/LearnWithMosh1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnwithMosh
+{
+    public static class Program
+    {
+        public static int width;
+        public static int height;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Portrait or Landscape Calculator");
+
+            Console.Write("Width of the image: ");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Height of the image: ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            Calculator.PortOrLand();
+
+        }
+    }
+}
