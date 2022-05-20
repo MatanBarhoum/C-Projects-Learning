dd688795b3148316947a748836aa533f24bce347
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-05-20 13:48:11 +0300

    Add files via upload

---
+++ Recrusion C#/FactorialRecrusion.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number to factorial: ");
+            var result = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Result is {0}", Factorial(result));
+            Console.ReadLine();
+        }
+        static double Factorial(int number)
+        {
+           if (number == 0)
+           {
+               return 1;
+          }
+            return number * Factorial(number - 1);
+            
+        }
+        }
+    }
+
